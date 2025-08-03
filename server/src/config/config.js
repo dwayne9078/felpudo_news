21dd8cb76f63d7774ee5610786a0b430304dcfcd
Dwayne9078 <pabloroo87@gmail.com>
2025-08-03 01:13:22 -0600

    fix: Load .env variables correctly

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,3 +1,7 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
 export const PORT = process.env.API_PORT;
 export const MONGO_URI = process.env.MONGO_URI;
 export const COOKIE_SECRET = process.env.COOKIE_SECRET;
