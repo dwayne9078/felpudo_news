2b74117e16116f08d5e359f1370ce3bf94446cfe
Dwayne9078 <pabloroo87@gmail.com>
2025-08-03 23:01:24 -0600

    refactor: Added Register Route

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { login, logout } from "../controllers/auth.controller.js";
+import { login, logout, register } from "../controllers/auth.controller.js";
 import Tokenizer from "../util/tokenizer.js";
 
 const authRouter = Router();
 
 authRouter.post("/login", login);
+authRouter.post("/register", register);
 authRouter.post("/logout", Tokenizer.verifyToken, logout);
 
 export default authRouter;
