8c05513f18cf4bd6a7550c9910def57ef8fe8f3b
Dwayne9078 <pabloroo87@gmail.com>
2025-08-06 00:49:45 -0600

    feat: Get A Specific News By Id

--- server/src/controllers/news.controller.js
+++ server/src/controllers/news.controller.js
@@ -5,7 +5,22 @@ export const getAllNews = async (req, res) => {
   return res.status(200).json({ news });
 };
 
-export const getNewsById = () => {};
+export const getNewsById = async (req, res) => {
+  try {
+    const newsId = req.params.id;
+
+    const news = await News.findById(newsId);
+
+    if (!news) {
+      return res.status(404).json({ message: "News not found" });
+    }
+
+    res.status(200).json(news);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 export const createNews = () => {};
 export const modifyNews = () => {};
 export const deleteNews = () => {};
