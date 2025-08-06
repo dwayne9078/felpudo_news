349f876b94315ef395ae427a03bab893310f55ef
Dwayne9078 <pabloroo87@gmail.com>
2025-08-05 18:48:40 -0600

    feat: Add unique, required and default attributes to User model

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,15 +1,31 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
-  password: {
-    type: String,
-    trim: true,
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("user", userSchema);
 
