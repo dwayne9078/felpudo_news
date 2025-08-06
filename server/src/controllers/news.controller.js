e8d24b81bfe4cec39a11fb757f119c27b93aff41
Dwayne9078 <pabloroo87@gmail.com>
2025-08-06 00:43:19 -0600

    feat: Get All News function

--- server/src/controllers/news.controller.js
+++ server/src/controllers/news.controller.js
@@ -1,6 +1,10 @@
 import News from "../models/news.model.js";
 
-export const getAllNews = () => {};
+export const getAllNews = async (req, res) => {
+  const news = await News.find();
+  return res.status(200).json({ news });
+};
+
 export const getNewsById = () => {};
 export const createNews = () => {};
 export const modifyNews = () => {};
