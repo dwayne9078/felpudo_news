db3b5271d9ed44799903de7e6f9fb7d2ecefece6
Dwayne9078 <pabloroo87@gmail.com>
2025-08-05 23:21:37 -0600

    feat: Create News Controller

---
+++ server/src/controllers/news.controller.js
@@ -0,0 +1,7 @@
+import News from "../models/news.model.js";
+
+export const getNews = () => {};
+export const getANews = () => {};
+export const createANews = () => {};
+export const modifyANews = () => {};
+export const deleteANews = () => {};
