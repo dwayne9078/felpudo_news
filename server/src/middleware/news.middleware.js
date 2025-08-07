3b1b689d116f63b679d4f278cc0b97d94ad350d0
Dwayne9078 <pabloroo87@gmail.com>
2025-08-06 19:01:34 -0600

    feat: Add News Id Validation

--- server/src/middleware/news.middleware.js
+++ server/src/middleware/news.middleware.js
@@ -1,4 +1,10 @@
 import { body, param } from "express-validator";
 
-export const validateNewsIdParam = [];
+export const validateNewsIdParam = [
+  param("id")
+    .notEmpty()
+    .withMessage("News ID is required")
+    .isMongoId()
+    .withMessage("Invalid News ID"),
+];
 export const validateNewsBody = [];
